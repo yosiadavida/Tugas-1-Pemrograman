7a608c596c5b91305303ed3c70075459a43bd965
yosiadavida <119830385+yosiadavida@users.noreply.github.com>
2026-05-10 23:03:33 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+    String nama;
+    int harga;
+    String kategori;
+
+    public Menu(String nama, int harga, String kategori) {
+        this.nama = nama;
+        this.harga = harga;
+        this.kategori = kategori;
+    }
+}
